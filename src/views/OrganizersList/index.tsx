4181b3f938872560cf11ee38d35387e814273a3d
manulon <mlongoe@fi.uba.ar>
2023-04-23 15:36:29 -0300

    fix: datagrid in spanish

--- src/views/OrganizersList/index.tsx
+++ src/views/OrganizersList/index.tsx
@@ -13,10 +13,10 @@ const OrganizersList: FC<IOrganizersListProps> = (props: IOrganizersListProps) =
       field: 'id', headerName: 'id', width: 50,
     },
     {
-      field: 'name', headerName: 'Name', width: 600,
+      field: 'name', headerName: 'Nombre', width: 600,
     },
     {
-      field: 'number_of_events', headerName: 'events', width: 400,
+      field: 'number_of_events', headerName: 'Cantidad de eventos', width: 400,
     },
 
   ];
@@ -25,11 +25,49 @@ const OrganizersList: FC<IOrganizersListProps> = (props: IOrganizersListProps) =
 
   const rowsWithIds: IOrganizer[] = addIds(rows);
 
+  /* eslint-disable */
   return (
     <div style={{ height: 600, width: 1100 }}>
-      <DataGrid rows={rowsWithIds} columns={columns} getRowId={(row) => row.id }/>
+      <DataGrid rows={rowsWithIds} columns={columns} getRowId={(row) => row.id } hideFooter={true}
+                localeText = {{ columnMenuSortDesc: 'Ordenar DESC',
+                columnMenuSortAsc: 'Ordenar ASC',
+                columnMenuFilter: 'Filtrar',
+                columnMenuHideColumn: 'Ocultar columna',
+                columnMenuManageColumns: 'Administrar columnas',
+                columnMenuUnsort: 'Desordenar',
+                filterPanelAddFilter: 'Agregar filtro',
+                filterPanelRemoveAll: 'Eliminar todos',
+                filterPanelDeleteIconLabel: 'Eliminar',
+                filterPanelLogicOperator: 'Operador logico',
+                filterPanelOperator: 'Operador',
+                filterPanelOperatorAnd: 'Y',
+                filterPanelOperatorOr: 'O',
+                filterPanelColumns: 'Columnas',
+                filterPanelInputLabel: 'Valor',
+                filterPanelInputPlaceholder: 'Valor del filtro',
+                filterOperatorContains: 'contiene',
+                filterOperatorEquals: 'igual a',
+                filterOperatorStartsWith: 'comienza por',
+                filterOperatorEndsWith: 'termina con',
+                filterOperatorIs: 'es',
+                filterOperatorNot: 'no es',
+                filterOperatorAfter: 'está despues de',
+                filterOperatorOnOrAfter: 'está en o está despues de',
+                filterOperatorBefore: 'está antes de',
+                filterOperatorOnOrBefore: 'está en o está antes de',
+                filterOperatorIsEmpty: 'es vacio',
+                filterOperatorIsNotEmpty: 'no está vacio',
+                filterOperatorIsAnyOf: 'contiene al menos',
+                columnsPanelTextFieldLabel: 'Encontrar columna',
+                columnsPanelTextFieldPlaceholder: 'Titulo de columna',
+                columnsPanelDragIconLabel: 'Reordenar columna',
+                columnsPanelShowAllButton: 'Mostrar todas',
+                columnsPanelHideAllButton: 'Ocultar todas',
+               }}
+      />
     </div>
   );
+  /* eslint-enable */
 };
 
 export default OrganizersList;
