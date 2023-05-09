1dd48ef5ea23ce93f8554b13fddaae3bad5bf163
manulon <mlongoe@fi.uba.ar>
2023-05-09 18:40:14 -0300

    [BWA-84]. task finished

--- src/containers/ReportsByUserContainer.tsx
+++ src/containers/ReportsByUserContainer.tsx
@@ -1,18 +1,19 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, useState } from 'react';
 import useTypedSelector from '../hooks/useTypedSelector';
 import ReportsByUserList from '../views/ReportsByUserList';
 import Layout from '../views/Layout';
 
 const ReportsByUserContainer: FunctionComponent = () => {
   const { reports } = useTypedSelector((state) => state.user);
-
-  console.log('estoy en report by user container');
-  console.log(reports);
+  const [showDescription, setShowDescription] = useState(false);
+  const [description, setDescription] = useState('');
 
   return (
     <Layout>
         {(
-          <ReportsByUserList reports={reports}/>
+          <ReportsByUserList reports={reports} showDescription={showDescription}
+          setShowDescription={setShowDescription} description={description}
+          setDescription={setDescription}/>
         )}
     </Layout>
   );
--- src/views/ReportsByUserList/styles.ts
+++ src/views/ReportsByUserList/styles.ts
@@ -1,16 +1,11 @@
 import styled from 'styled-components';
 import COLORS from '../../helpers/colors';
 
-export const BlockButton = styled.div`
+export const ViewDescriptionButton = styled.div`
   text-align: center;
   margin: 0;
-  border-radius: 20px;
-  border: 1px solid;
-  border-color: ${COLORS.violetScampi};
-  color: ${COLORS.black};
-  font-weight: bold;
+  color: ${COLORS.linkBlue};
   width: 100px;
-  padding: 8px;
   cursor: pointer;
 `;
 
